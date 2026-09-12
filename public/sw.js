05dba83b40aafc857b0228ecc4b2a8fed2345c43
sjcoo15 <166068425+sjcoo15@users.noreply.github.com>
2026-09-13 04:41:45 +1000

    Refresh PWA cache for updated exercise images

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,11 @@
-const CACHE='core-trainer-v5';
+const CACHE='core-trainer-v6';
 const APP=['/','/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-512-maskable.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request).then(response=>{
+  const request=event.request.destination==='image' ? new Request(event.request,{cache:'no-store'}) : event.request;
+  event.respondWith(fetch(request).then(response=>{
     const copy=response.clone();
     caches.open(CACHE).then(cache=>cache.put(event.request,copy));
     return response;
