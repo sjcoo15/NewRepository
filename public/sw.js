8ef37f7259139500bfdea15b80778a2b39c756f3
sjcoo15 <166068425+sjcoo15@users.noreply.github.com>
2026-09-13 04:08:35 +1000

    Refresh PWA cache after repairing icons

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='core-trainer-v3';
+const CACHE='core-trainer-v4';
 const APP=['/','/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-512-maskable.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
