d4646120109b61e91fbe67e91e77581ef48ed883
sjcoo15 <166068425+sjcoo15@users.noreply.github.com>
2026-09-13 04:04:17 +1000

    Force service worker updates for PWA installability

--- app/pwa-register.js
+++ app/pwa-register.js
@@ -4,7 +4,9 @@ import { useEffect } from 'react';
 export default function PwaRegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js', { scope: '/' }).catch(() => {});
+      navigator.serviceWorker.register('/sw.js', { scope: '/' })
+        .then(registration => registration.update())
+        .catch(() => {});
     }
   }, []);
   return null;
